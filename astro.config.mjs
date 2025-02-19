89901a653f4fcc5854debe9959e0b295196f40c5
Jonny McKirgan <jonathanmckirgan@gmail.com>
2025-02-19 22:54:46 +0000

    First commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+
+export default defineConfig({
+  site: 'https://jmckirgan.github.io',
+  base: 'my-repo',
+})
